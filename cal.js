ec480eec12cde973e6d7a014d98c5d5cfe77e3d8
David <daveedtee5@gmail.com>
2025-03-02 23:41:38 -0800

    fax

--- cal.js
+++ cal.js
@@ -1,8 +1,39 @@
 const Numbers = document.querySelectorAll('#Number');
+const del = document.querySelector('#clear');
+const operations = document.querySelectorAll('#operator');
+const result = document.querySelector('.result');
+const equal = document.querySelector('#showResult');
+const decimaladd = document.querySelector('#decimal');
+const percent = document.querySelector('#percentage');
+const negativepositive = document.querySelector('#negative');
 
 Numbers.forEach(button => { 
     button.addEventListener('click', function() {
-        const result = document.querySelector('.result');
         result.value += button.innerHTML;
     });
 });
+
+del.addEventListener('click', function() {
+    result.value = '';
+});
+
+operations.forEach(button => {
+    button.addEventListener('click', function() {
+        if (result.value !== '') {
+            const lastChar = result.value.slice(-1); 
+            const currentOperator = button.innerHTML === 'X' ? '*' : button.innerHTML; 
+
+            if (isOperator(lastChar)) {
+                result.value = result.value.slice(0, -1) + currentOperator;
+            } else {
+                result.value += currentOperator;
+            }
+        } else {
+            console.log('No number to operate on');
+        }
+    });
+});
+
+function isOperator(character) {
+    return ['+', '-', '*', '/'].includes(character);
+}
